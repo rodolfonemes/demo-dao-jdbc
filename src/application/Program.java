4c9994d1dd4440d557fb6d55fcfabf973b33e56f
Rodolfo Nemes <tecnico.rodolfoo@gmail.com>
2022-10-01 14:50:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
 
+		Seller seller = new Seller(21, "Rodolfo", "tecnico.rodolfoo@gmail.com", new Date(), 2500.00, obj);
+		System.out.println();
+		System.out.println(seller);
 	}
 
 }
